e6dcb6650a2d3f0ff3a64bd0be9b9876b168b7da
tj335 <105184198+tj335@users.noreply.github.com>
2022-09-07 21:40:00 -0400

    Update ha-nfl-card.js

--- dist/ha-nfl-card.js
+++ dist/ha-nfl-card.js
@@ -96,14 +96,14 @@ class NFLCard extends LitElement {
       return html`
         <style>
           .card { position: relative; overflow: hidden; padding: 16px 16px 20px; font-weight: 400; }
-          .team-bg { opacity: 0.08; position: absolute; top: -30%; left: -20%; width: 58%; z-index: 0; }
-          .opponent-bg { opacity: 0.08; position: absolute; top: -30%; right: -20%; width: 58%; z-index: 0; }
+          .away-team-bg { opacity: 0.08; position: absolute; top: -30%; left: -20%; width: 58%; z-index: 0; }
+          .home-team-bg { opacity: 0.08; position: absolute; top: -30%; right: -20%; width: 58%; z-index: 0; }
           .card-content { display: flex; justify-content: space-evenly; align-items: center; text-align: center; position: relative; z-index: 99; }
           .team { text-align: center; width: 35%;}
           .team img { max-width: 90px; }
           .score { font-size: 3em; text-align: center; }
-          .teamscr { opacity: ${teamScore}; }
-          .opposcr { opacity: ${oppoScore}; }
+          .awayteamscr { opacity: ${awayTeamScore}; }
+          .hometeamscr { opacity: ${homeTeamScore}; }
           .divider { font-size: 2.5em; text-align: center; opacity: 0; }
           .name { font-size: 1.4em; margin-bottom: 4px; }
           .line { height: 1px; background-color: var(--primary-text-color); margin:10px 0; }
@@ -111,21 +111,21 @@ class NFLCard extends LitElement {
         </style>
         <ha-card>
           <div class="card">
-            <img class="team-bg" src="${stateObj.attributes.team_logo}" />
-            <img class="opponent-bg" src="${stateObj.attributes.opponent_logo}" />
+            <img class="away-team-bg" src="${stateObj.attributes.away_team_logo}" />
+            <img class="home-team-bg" src="${stateObj.attributes.home_team_logo}" />
             <div class="card-content">
               <div class="team">
-                <img src="${stateObj.attributes.team_logo}" />
-                <div class="name">${stateObj.attributes.team_name}</div>
-                <div class="record">${stateObj.attributes.team_record}</div>
+                <img src="${stateObj.attributes.away_team_logo}" />
+                <div class="name">${stateObj.attributes.away_team_name}</div>
+                <div class="record">${stateObj.attributes.away_team_record}</div>
               </div>
-              <div class="score teamscr">${tScr}</div>
+              <div class="score awayteamscr">${aScr}</div>
               <div class="divider">-</div>
-              <div class="score opposcr">${oScr}</div>
+              <div class="score hometeamscr">${hScr}</div>
               <div class="team">
-                <img src="${stateObj.attributes.opponent_logo}" />
-                <div class="name">${stateObj.attributes.opponent_name}</div>
-                <div class="record">${stateObj.attributes.opponent_record}</div>
+                <img src="${stateObj.attributes.home_team_logo}" />
+                <div class="name">${stateObj.attributes.home_team_name}</div>
+                <div class="record">${stateObj.attributes.home_team_record}</div>
               </div>
             </div>
             <div class="status">${gameMonth} ${gameDate} - FINAL</div>
@@ -139,14 +139,14 @@ class NFLCard extends LitElement {
         return html`
           <style>
             .card { position: relative; overflow: hidden; padding: 0 16px 20px; font-weight: 400; }
-            .team-bg { opacity: 0.08; position:absolute; top: -20%; left: -20%; width: 58%; z-index: 0; }
-            .opponent-bg { opacity: 0.08; position:absolute; top: -20%; right: -20%; width: 58%; z-index: 0; }
+            .away-team-bg { opacity: 0.08; position:absolute; top: -20%; left: -20%; width: 58%; z-index: 0; }
+            .home-team-bg { opacity: 0.08; position:absolute; top: -20%; right: -20%; width: 58%; z-index: 0; }
             .card-content { display: flex; justify-content: space-evenly; align-items: center; text-align: center; position: relative; z-index: 99; }
             .team { text-align: center; width:35%; }
             .team img { max-width: 90px; }
-            .possession, .teamposs, .oppoposs { font-size: 2.5em; text-align: center; opacity: 0; font-weight:900; }
-            .teamposs {opacity: ${teamPoss} !important; }
-            .oppoposs {opacity: ${oppoPoss} !important; }
+            .possession, .awayteamposs, .hometeamposs { font-size: 2.5em; text-align: center; opacity: 0; font-weight:900; }
+            .awayteamposs {opacity: ${awayTeamPoss} !important; }
+            .hometeamposs {opacity: ${homeTeamPoss} !important; }
             .score { font-size: 3em; text-align: center; }
             .divider { font-size: 2.5em; text-align: center; margin: 0 4px; }
             .name { font-size: 1.4em; margin-bottom: 4px; }
@@ -154,8 +154,8 @@ class NFLCard extends LitElement {
             .timeouts { margin: 0 auto; width: 70%; }
             .timeouts div.opponent-to:nth-child(-n + ${stateObj.attributes.opponent_timeouts})  { opacity: 1; }
             .timeouts div.team-to:nth-child(-n + ${stateObj.attributes.team_timeouts})  { opacity: 1; }
-            .team-to { height: 6px; border-radius: ${toRadius}px; border: ${clrOut}px solid ${outColor}; width: 20%; background-color: ${teamColor}; display: inline-block; margin: 0 auto; position: relative; opacity: 0.2; }
-            .opponent-to { height: 6px; border-radius: ${toRadius}px; border: ${clrOut}px solid ${outColor}; width: 20%; background-color: ${oppoColor}; display: inline-block; margin: 0 auto; position: relative; opacity: 0.2; }
+            .away-team-to { height: 6px; border-radius: ${toRadius}px; border: ${clrOut}px solid ${outColor}; width: 20%; background-color: ${awayTeamColor}; display: inline-block; margin: 0 auto; position: relative; opacity: 0.2; }
+            .home-team-to { height: 6px; border-radius: ${toRadius}px; border: ${clrOut}px solid ${outColor}; width: 20%; background-color: ${homeTeamColor}; display: inline-block; margin: 0 auto; position: relative; opacity: 0.2; }
             .status { text-align:center; font-size:1.6em; font-weight: 700; }
             .sub1 { font-weight: 700; font-size: 1.2em; margin: 6px 0 2px; }
             .sub1, .sub2, .sub3 { display: flex; justify-content: space-between; align-items: center; margin: 2px 0; }
@@ -167,53 +167,53 @@ class NFLCard extends LitElement {
             .play-clock { font-size: 1.4em; text-align: center; margin-top: -24px; }
             .probability-text { text-align: center; }
             .prob-flex { width: 100%; display: flex; justify-content: center; margin-top: 4px; }
-            .opponent-probability { width: ${oppoProb}%; background-color: ${oppoColor}; height: 12px; border-radius: 0 ${probRadius}px ${probRadius}px 0; border: ${clrOut}px solid ${outColor}; border-left: 0; transition: all 1s ease-out; }
-            .team-probability { width: ${teamProb}%; background-color: ${teamColor}; height: 12px; border-radius: ${probRadius}px 0 0 ${probRadius}px; border: ${clrOut}px solid ${outColor}; border-right: 0; transition: all 1s ease-out; }
+            .home-team-probability { width: ${homeTeamProb}%; background-color: ${homeTeamColor}; height: 12px; border-radius: 0 ${probRadius}px ${probRadius}px 0; border: ${clrOut}px solid ${outColor}; border-left: 0; transition: all 1s ease-out; }
+            .away-team-probability { width: ${awayTeamProb}%; background-color: ${awayTeamColor}; height: 12px; border-radius: ${probRadius}px 0 0 ${probRadius}px; border: ${clrOut}px solid ${outColor}; border-right: 0; transition: all 1s ease-out; }
             .probability-wrapper { display: flex; }
-            .team-percent { flex: 0 0 10px; padding: 0 10px 0 0; }
-            .oppo-percent { flex: 0 0 10px; padding: 0 0 0 10px; text-align: right; }
+            .away-team-percent { flex: 0 0 10px; padding: 0 10px 0 0; }
+            .home-team-percent { flex: 0 0 10px; padding: 0 0 0 10px; text-align: right; }
             .percent { padding: 0 6px; }
             .post-game { margin: 0 auto; }
           </style>
           <ha-card>
             <div class="card">
-            <img class="team-bg" src="${stateObj.attributes.team_logo}" />
-            <img class="opponent-bg" src="${stateObj.attributes.opponent_logo}" />
+            <img class="away-team-bg" src="${stateObj.attributes.away_team_logo}" />
+            <img class="home-team-bg" src="${stateObj.attributes.home_team_logo}" />
             <div class="card-content">
               <div class="team">
-                <img src="${stateObj.attributes.team_logo}" />
-                <div class="name">${stateObj.attributes.team_name}</div>
-                <div class="record">${stateObj.attributes.team_record}</div>
+                <img src="${stateObj.attributes.away_team_logo}" />
+                <div class="name">${stateObj.attributes.away_team_name}</div>
+                <div class="record">${stateObj.attributes.away_team_record}</div>
                 <div class="timeouts">
-                  <div class="team-to"></div>
-                  <div class="team-to"></div>
-                  <div class="team-to"></div>
+                  <div class="away-team-to"></div>
+                  <div class="away-team-to"></div>
+                  <div class="away-team-to"></div>
                 </div>
               </div>
-              <div class="teamposs">&bull;</div>
-              <div class="score">${stateObj.attributes.team_score}</div>
+              <div class="awayteamposs">&bull;</div>
+              <div class="score">${stateObj.attributes.away_team_score}</div>
               <div class="divider">-</div>
-              <div class="score">${stateObj.attributes.opponent_score}</div>
-              <div class="oppoposs">&bull;</div>
+              <div class="score">${stateObj.attributes.home_team_score}</div>
+              <div class="hometeamposs">&bull;</div>
               <div class="team">
-                <img src="${stateObj.attributes.opponent_logo}" />
-                <div class="name">${stateObj.attributes.opponent_name}</div>
-                <div class="record">${stateObj.attributes.opponent_record}</div>
+                <img src="${stateObj.attributes.home_team_logo}" />
+                <div class="name">${stateObj.attributes.home_team_name}</div>
+                <div class="record">${stateObj.attributes.home_team_record}</div>
                 <div class="timeouts">
-                  <div class="opponent-to"></div>
-                  <div class="opponent-to"></div>
-                  <div class="opponent-to"></div>
+                  <div class="home-team-to"></div>
+                  <div class="home-team-to"></div>
+                  <div class="home-team-to"></div>
                 </div>
               </div>
             </div>
             <div class="play-clock">Q${stateObj.attributes.quarter} - ${stateObj.attributes.clock}</div>
             <div class="line"></div>
             <div class="sub2">
-              <div class="venue">${stateObj.attributes.venue}</div>
+              <div class="venue">${stateObj.attributes.venue_name}</div>
              <div class="down-distance">${stateObj.attributes.down_distance_text}</div>
             </div>
             <div class="sub3">
-              <div class="location">${stateObj.attributes.location}</div>
+              <div class="location">${stateObj.attributes.venue_city}, ${stateObj.attributes.venue_state}</div>
               <div class="network">${stateObj.attributes.tv_network}</div>
             </div>
             <div class="line"></div>
@@ -222,12 +222,12 @@ class NFLCard extends LitElement {
             </div>
             <div class="probability-text">Win Probability</div>
             <div class="probability-wrapper">
-              <div class="team-percent">${teamProb}%</div>
+              <div class="away-team-percent">${awayTeamProb}%</div>
               <div class="prob-flex">
-                <div class="team-probability"></div>
-                <div class="opponent-probability"></div>
+                <div class="away-team-probability"></div>
+                <div class="home-team-probability"></div>
               </div>
-              <div class="oppo-percent">${oppoProb}%</div>
+              <div class="home-team-percent">${homeTeamProb}%</div>
             </div>
           </div>
           </ha-card>
@@ -238,8 +238,8 @@ class NFLCard extends LitElement {
         return html`
           <style>
             .card { position: relative; overflow: hidden; padding: 0 16px 20px; font-weight: 400; }
-            .team-bg { opacity: 0.08; position:absolute; top: -20%; left: -20%; width: 58%; z-index: 0; }
-            .opponent-bg { opacity: 0.08; position:absolute; top: -20%; right: -20%; width: 58%; z-index: 0; }
+            .away-team-bg { opacity: 0.08; position:absolute; top: -20%; left: -20%; width: 58%; z-index: 0; }
+            .home-team-bg { opacity: 0.08; position:absolute; top: -20%; right: -20%; width: 58%; z-index: 0; }
             .card-content { display: flex; justify-content: space-evenly; align-items: center; text-align: center; position: relative; z-index: 99; }
             .team { text-align: center; width: 35%; }
             .team img { max-width: 90px; }
@@ -258,22 +258,22 @@ class NFLCard extends LitElement {
           </style>
           <ha-card>
               <div class="card">
-              <img class="team-bg" src="${stateObj.attributes.team_logo}" />
-              <img class="opponent-bg" src="${stateObj.attributes.opponent_logo}" />
+              <img class="away-team-bg" src="${stateObj.attributes.away_team_logo}" />
+              <img class="home-team-bg" src="${stateObj.attributes.home_team_logo}" />
               <div class="card-content">
                 <div class="team">
-                  <img src="${stateObj.attributes.team_logo}" />
-                  <div class="name">${stateObj.attributes.team_name}</div>
-                  <div class="record">${stateObj.attributes.team_record}</div>
+                  <img src="${stateObj.attributes.away_team_logo}" />
+                  <div class="name">${stateObj.attributes.away_team_name}</div>
+                  <div class="record">${stateObj.attributes.away_team_record}</div>
                 </div>
                 <div class="gamewrapper">
                   <div class="gameday">${gameDay}</div>
                   <div class="gametime">${gameTime}</div>
                 </div>
                 <div class="team">
-                  <img src="${stateObj.attributes.opponent_logo}" />
-                  <div class="name">${stateObj.attributes.opponent_name}</div>
-                  <div class="record">${stateObj.attributes.opponent_record}</div>
+                  <img src="${stateObj.attributes.home_team_logo}" />
+                  <div class="name">${stateObj.attributes.home_team_name}</div>
+                  <div class="record">${stateObj.attributes.home_team_record}</div>
                 </div>
               </div>
               <div class="line"></div>
@@ -282,11 +282,11 @@ class NFLCard extends LitElement {
                 <div class="odds">${stateObj.attributes.odds}</div>
               </div>
               <div class="sub2">
-                <div class="venue">${stateObj.attributes.venue}</div>
+                <div class="venue">${stateObj.attributes.venue_name}</div>
                 <div class="overunder"> O/U: ${stateObj.attributes.overunder}</div>
               </div>
               <div class="sub3">
-                <div class="location">${stateObj.attributes.location}</div>
+                <div class="location">${stateObj.attributes.venue_city}, ${stateObj.attributes.venue_state}</div>
                 <div class="network">${stateObj.attributes.tv_network}</div>
               </div>
             </div>
