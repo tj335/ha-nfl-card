46f1cbf5568d4c2e390de4d9debf6dc05d3caa1e
tj335 <105184198+tj335@users.noreply.github.com>
2022-11-16 21:40:39 -0500

    Fix for Weather in Pre Game

--- dist/ha-nfl-card.js
+++ dist/ha-nfl-card.js
@@ -61,11 +61,11 @@ class NFLCard extends LitElement {
     var outColor = outlineColor;
 
     if (stateObj.attributes.venue_indoor) {
-      if (stateObj.attributes.venue_indoor == true) {
+      if (stateObj.attributes.venue_indoor == 'true') {
         weather = 'Indoors';
       } else {
         if (stateObj.attributes.weather_conditions && stateObj.attributes.weather_temp) {
-          weather = stateObj.attributes.weather_conditions + ', ' + stateObj.attributes.weather_temp;
+          weather = stateObj.attributes.weather_conditions + ', ' + stateObj.attributes.weather_temp + '°F';
         } else {
           weather = 'Weather Unavailable';
         }
